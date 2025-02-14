2b0eaf04b64cbed86e418a56719fc3693de28215
Aditya <gupta.aditya2309@gmail.com>
2025-02-14 11:11:44 +0530

    shelter

--- frontend/src/pages/shelter_dash.jsx
+++ frontend/src/pages/shelter_dash.jsx
@@ -1,6 +1,11 @@
-function Shelter_dash(){
+import { Link } from "react-router-dom";
+
+function Shelter_dash() {
   return (
-    <button><Link to="/post" style={{color: "#00d4ff"}}>Add a post</Link></button>
+    <button>
+      <Link to="/add_post" style={{ color: "#00d4ff" }}>Add a post</Link>
+    </button>
   );
 }
+
 export default Shelter_dash;
